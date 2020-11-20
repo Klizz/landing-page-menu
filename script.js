82d0dd56ad42d49ea3b608a7a2edde4eb035c8c0
Karen <klizzy@hotmail.es>
2020-11-20 17:42:52 -0600

    finished page

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function openNav() {
+  document
+    .getElementsByClassName("nav-links")[0]
+    .classList.remove("hidden-nav");
   document.getElementById("sideNav").style.width = "80%";
   document.body.style.backgroundColor = "rgb(0,0,0, 0.25)";
   document.getElementById("openBtn").style.display = "none";
@@ -10,17 +13,20 @@ function openNav() {
 }
 
 function closeNav() {
+  document.getElementsByClassName("nav-links")[0].classList.add("hidden-nav");
   document.getElementById("sideNav").style.width = "0";
   document.body.style.backgroundColor = "white";
   document.getElementById("openBtn").style.display = "initial";
 }
 
 document.addEventListener("click", function (event) {
-    var isClickInside = document.getElementById("sideNav").contains(event.target);
-    if (isClickInside) {
-      closeNav();
-    }
-  });
+  var isClickInside = document
+    .getElementById("backDrop")
+    .contains(event.target);
+  if (isClickInside) {
+    closeNav();
+  }
+});
 
 function addEvent(element, eventName, callback) {
   if (element.addEventListener) {
